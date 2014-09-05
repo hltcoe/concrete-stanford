86b6c3c8d80b5d49cf148d5de31ef9285ae63262
Max Thomas <maxjthomas@gmail.com>
2014-09-05 16:11:54 -0400

    Kill legacy class.

--- src/main/java/edu/jhu/hlt/concrete/util/UUIDGenerator.java
+++
@@ -1,9 +0,0 @@
-package edu.jhu.hlt.concrete.util;
-
-import java.util.UUID;
-
-public class UUIDGenerator {
-    public static String make(){
-        return UUID.randomUUID().toString();
-    }
-}
