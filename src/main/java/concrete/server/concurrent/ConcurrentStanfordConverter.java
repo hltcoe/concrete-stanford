23069ac60a6cde78663b79a61d3c3515aaf4a616
Max Thomas <max.thomas@jhuapl.edu>
2014-09-06 15:01:24 -0400

    Disable SSL.

--- src/main/java/concrete/server/concurrent/ConcurrentStanfordConverter.java
+++ src/main/java/concrete/server/concurrent/ConcurrentStanfordConverter.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  */
 package concrete.server.concurrent;
 
@@ -41,29 +41,29 @@ import edu.jhu.hlt.gigaword.ProxyDocument;
 public class ConcurrentStanfordConverter implements AutoCloseable {
 
   private static final Logger logger = LoggerFactory.getLogger(ConcurrentStanfordConverter.class);
-  
+
   private final ExecutorService runner;
   private final CompletionService<Communication> srv;
-  
+
   /**
-   * 
+   *
    */
   public ConcurrentStanfordConverter() {
     this.runner = Executors.newCachedThreadPool();
     this.srv = new ExecutorCompletionService<Communication>(this.runner);
   }
-  
+
   public Future<Communication> annotate(Future<Communication> fc) throws InterruptedException, ExecutionException {
     return this.srv.submit(new CallableConcreteServer(fc));
   }
-  
+
   public Future<Communication> annotate(Communication c) throws InterruptedException, ExecutionException {
     return this.srv.submit(new CallableConcreteServer(c));
   }
-  
+
   /**
    * @param args
-   * @throws Exception 
+   * @throws Exception
    */
   public static void main(String[] args) throws Exception {
 //    int threads = Runtime.getRuntime().availableProcessors();
@@ -71,17 +71,17 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
 //      logger.info("You need at least 8 threads to run this program. You only have {} available.", threads);
 //      System.exit(1);
 //    }
-    
+
     if (args.length != 1) {
       logger.info("This program takes 1 argument: the path to a .txt file with paths to agiga documents, 1 per line.");
       System.exit(1);
     }
-    
+
     Optional<String> psqlHost = Optional.ofNullable(System.getenv("HURRICANE_HOST"));
     Optional<String> psqlDBName = Optional.ofNullable(System.getenv("HURRICANE_DB"));
     Optional<String> psqlUser = Optional.ofNullable(System.getenv("HURRICANE_USER"));
     Optional<String> psqlPass = Optional.ofNullable(System.getenv("HURRICANE_PASS"));
-    
+
     if (!psqlHost.isPresent() || !psqlDBName.isPresent() || !psqlUser.isPresent() || !psqlPass.isPresent()) {
       logger.info("You need to set the following environment variables to run this program:");
       logger.info("HURRICANE_HOST : hostname of a postgresql server");
@@ -90,35 +90,35 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
       logger.info("HURRICANE_PASS : password for user");
       System.exit(1);
     }
-    
+
     Properties props = new Properties();
     props.setProperty("user", psqlUser.get());
     props.setProperty("password", psqlPass.get());
-    props.setProperty("ssl", "true");
+    // props.setProperty("ssl", "true");
     Connection conn = DriverManager.getConnection("jdbc:postgresql://" + psqlHost.get() + "/" + psqlDBName.get(), props);
     conn.setAutoCommit(false);
     logger.info("Successfully connected to database.");
-    
+
     CommunicationSerialization cs = new CommunicationSerialization();
-    
+
     logger.info("Sleeping to allow profiler hooks...");
     Thread.sleep(10000);
     logger.info("Proceeding.");
-    
+
     // this is silly, but needed for stanford logging disable.
     PrintStream err = System.err;
 
     System.setErr(new PrintStream(new OutputStream() {
       public void write(int b) { }
     }));
-    
+
     StopWatch sw = new StopWatch();
     logger.info("Ingest beginning at: {}", new DateTime().toString());
     Path pathToCommFiles = Paths.get(args[0]);
-    
+
     ClojureIngester ci = new ClojureIngester();
     ConcurrentStanfordConverter annotator = new ConcurrentStanfordConverter();
-    
+
     List<Future<Communication>> comms = new ArrayList<>();
     try(Scanner sc = new Scanner(pathToCommFiles.toFile())) {
       while (sc.hasNextLine()) {
@@ -150,12 +150,12 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
         ps.executeUpdate();
       }
     }
-    
+
     logger.info("Database transaction prepared. Committing.");
     conn.commit();
     logger.info("Committed successfully. Shutting down database connection.");
     conn.close();
-    
+
     sw.stop();
     logger.info("Ingest complete. Took {} ms.", sw.getTime());
     annotator.close();
