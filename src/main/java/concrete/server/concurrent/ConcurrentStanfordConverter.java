9ffa0c68a89ddcf7305faafb4373097ea5e6e750
Max Thomas <maxjthomas@gmail.com>
2014-09-07 21:14:12 -0400

    Better exception handling.

--- src/main/java/concrete/server/concurrent/ConcurrentStanfordConverter.java
+++ src/main/java/concrete/server/concurrent/ConcurrentStanfordConverter.java
@@ -3,8 +3,11 @@
  */
 package concrete.server.concurrent;
 
+import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -35,6 +38,7 @@ import org.slf4j.LoggerFactory;
 
 import edu.jhu.hlt.concrete.Communication;
 import edu.jhu.hlt.concrete.util.CommunicationSerialization;
+import edu.jhu.hlt.concrete.util.ConcreteException;
 import edu.jhu.hlt.gigaword.ClojureIngester;
 import edu.jhu.hlt.gigaword.ProxyDocument;
 
@@ -54,9 +58,9 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
    */
   public ConcurrentStanfordConverter() {
     this.runner = Executors.newCachedThreadPool();
-//    int aThreads = Runtime.getRuntime().availableProcessors();
-//    int toUse = aThreads > 16 ? aThreads - 8 : aThreads;
-//    this.runner = Executors.newFixedThreadPool(toUse);
+    // int aThreads = Runtime.getRuntime().availableProcessors();
+    // int toUse = aThreads > 16 ? aThreads - 8 : aThreads;
+    // this.runner = Executors.newFixedThreadPool(toUse);
     this.srv = new ExecutorCompletionService<Communication>(this.runner);
   }
 
@@ -72,15 +76,30 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
    * @param args
    * @throws Exception
    */
-  public static void main(String[] args) throws Exception {
-//    int threads = Runtime.getRuntime().availableProcessors();
-//    if (threads < 8) {
-//      logger.info("You need at least 8 threads to run this program. You only have {} available.", threads);
-//      System.exit(1);
-//    }
+  public static void main(String[] args) {
+    // int threads = Runtime.getRuntime().availableProcessors();
+    // if (threads < 8) {
+    // logger.info("You need at least 8 threads to run this program. You only have {} available.", threads);
+    // System.exit(1);
+    // }
 
     if (args.length != 1) {
       logger.info("This program takes 1 argument: the path to a .txt file with paths to agiga documents, 1 per line.");
+      System.exit(1);
+    }
+    
+    logger.info("Setting up uncaught exception handler.");
+    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+      @Override
+      public void uncaughtException(Thread t, Throwable e) {
+        logger.error("Caught unhandled exception in thread: [{}]", t.getName());
+        logger.error("Exception is as follows.", e);
+      }
+    });
+    
+    Path pathToCommFiles = Paths.get(args[0]);
+    if (!Files.exists(pathToCommFiles)) {
+      logger.error("No file at: {} ; can't ingest anything.", pathToCommFiles.toString());
       System.exit(1);
     }
 
@@ -88,7 +107,7 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
     Optional<String> psqlDBName = Optional.ofNullable(System.getenv("HURRICANE_DB"));
     Optional<String> psqlUser = Optional.ofNullable(System.getenv("HURRICANE_USER"));
     Optional<String> psqlPass = Optional.ofNullable(System.getenv("HURRICANE_PASS"));
-    
+
     if (!psqlHost.isPresent() || !psqlDBName.isPresent() || !psqlUser.isPresent() || !psqlPass.isPresent()) {
       logger.info("You need to set the following environment variables to run this program:");
       logger.info("HURRICANE_HOST : hostname of a postgresql server");
@@ -102,98 +121,119 @@ public class ConcurrentStanfordConverter implements AutoCloseable {
     props.setProperty("user", psqlUser.get());
     props.setProperty("password", psqlPass.get());
     // props.setProperty("ssl", "true");
-    Connection conn = DriverManager.getConnection("jdbc:postgresql://" + psqlHost.get() + "/" + psqlDBName.get(), props);
-    conn.setAutoCommit(false);
-    logger.info("Successfully connected to database. Getting previously ingested IDs.");
-    
-    Set<String> idSet = new HashSet<>();
-    try (PreparedStatement ps = conn.prepareStatement("SELECT id FROM documents");) {
-      ResultSet rs = ps.executeQuery();
-      while (rs.next()) {
-        String id = rs.getString(1);
-        idSet.add(id);
-      }
-    }
+    try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + psqlHost.get() + "/" + psqlDBName.get(), props);) {
+      conn.setAutoCommit(false);
+      logger.info("Successfully connected to database. Getting previously ingested IDs.");
 
-    logger.info("Got previously ingested IDs. There are {} previously ingested documents.", idSet.size());
-    CommunicationSerialization cs = new CommunicationSerialization();
-
-    logger.info("Sleeping to allow profiler hooks...");
-    Thread.sleep(10000);
-    logger.info("Proceeding.");
-
-    // this is silly, but needed for stanford logging disable.
-    PrintStream err = System.err;
-
-    System.setErr(new PrintStream(new OutputStream() {
-      public void write(int b) { }
-    }));
-
-    StopWatch sw = new StopWatch();
-    logger.info("Ingest beginning at: {}", new DateTime().toString());
-    Path pathToCommFiles = Paths.get(args[0]);
-
-    ClojureIngester ci = new ClojureIngester();
-    ConcurrentStanfordConverter annotator = new ConcurrentStanfordConverter();
-
-    List<String> pathStrs = new ArrayList<>();
-    try(Scanner sc = new Scanner(pathToCommFiles.toFile())) {
-      while (sc.hasNextLine())
-        pathStrs.add(sc.nextLine());
-    }
-  
-    int kProcessed = 0;
-    int kPending = 0;
-    for (String pathStr : pathStrs) {
-      logger.info("Processing file: {}", pathStr);
-      Iterator<ProxyDocument> iter = ci.proxyGZipPathToProxyDocIter(pathStr);
-      while (iter.hasNext()) {
-        ProxyDocument pd = iter.next();
-        String pId = pd.getId();
-        if (idSet.contains(pId)) {
-          logger.info("Already ingested document: {}; skipping.", pId);
-          continue;
+      Set<String> idSet = new HashSet<>();
+      try (PreparedStatement ps = conn.prepareStatement("SELECT id FROM documents");) {
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+          String id = rs.getString(1);
+          idSet.add(id);
         }
-        
-        Communication c = pd.sectionedCommunication();
-        annotator.annotate(c);
-        logger.debug("Task submitted.");
-        kPending++;
+      }
+
+      logger.info("Got previously ingested IDs. There are {} previously ingested documents.", idSet.size());
+      CommunicationSerialization cs = new CommunicationSerialization();
+
+      logger.info("Sleeping to allow profiler hooks...");
+      Thread.sleep(10000);
+      logger.info("Proceeding.");
+
+      // this is silly, but needed for stanford logging disable.
+      PrintStream err = System.err;
+
+      System.setErr(new PrintStream(new OutputStream() {
+        public void write(int b) {
+        }
+      }));
+
+      StopWatch sw = new StopWatch();
+      logger.info("Ingest beginning at: {}", new DateTime().toString());
+
+      ClojureIngester ci = new ClojureIngester();
+      ConcurrentStanfordConverter annotator = new ConcurrentStanfordConverter();
+
+      List<String> pathStrs = new ArrayList<>();
+      try (Scanner sc = new Scanner(pathToCommFiles.toFile())) {
+        while (sc.hasNextLine())
+          pathStrs.add(sc.nextLine());
+      } catch (FileNotFoundException fnfe) {
+        // Catch clause will only ever be reached if file is deleted between
+        // beginning of program execution and this line. (not likely)
+        logger.error("There was no file at: {}", pathToCommFiles.toString());
+        System.exit(1);
+      }
+
+      int kProcessed = 0;
+      int kPending = 0;
+      for (String pathStr : pathStrs) {
+        logger.info("Processing file: {}", pathStr);
+        Iterator<ProxyDocument> iter = ci.proxyGZipPathToProxyDocIter(pathStr);
+        while (iter.hasNext()) {
+          ProxyDocument pd = iter.next();
+          String pId = pd.getId();
+          if (idSet.contains(pId)) {
+            logger.info("Already ingested document: {}; skipping.", pId);
+            continue;
+          }
+
+          Communication c = pd.sectionedCommunication();
+          annotator.annotate(c);
+          logger.debug("Task submitted.");
+          kPending++;
+        }
+
+        logger.info("Tasks submitted. Preparing SQL inserts.");
+        while (kPending != 0) {
+          logger.info("Waiting on next document in driver...");
+          // c = annotator.srv.poll(60 * 3, TimeUnit.SECONDS);
+          Future<Communication> c = annotator.srv.poll(60 * 3, TimeUnit.SECONDS);
+          Communication ac = c.get();
+          logger.debug("Retrieved communication: {}", ac.getId());
+          kPending--;
+          try (PreparedStatement ps = conn.prepareStatement("INSERT INTO documents (id, bytez) VALUES (?,?)");) {
+            ps.setString(1, ac.getId());
+            ps.setBytes(2, cs.toBytes(ac));
+            ps.executeUpdate();
+            kProcessed++;
+
+            if (kProcessed % 100 == 0) {
+              logger.info("Converted {} documents; committing.", kProcessed);
+              conn.commit();
+            }
+          } catch (SQLException e) {
+            logger.error("Caught an SQLException inserting documents.", e);
+          } catch (ConcreteException e) {
+            logger.error("There was an error creating a byte array from communication: {}", ac.getId());
+            
+          }
+        }
+      }
+
+      logger.info("Database transaction prepared. Committing.");
+      conn.commit();
+      logger.info("Committed successfully. Shutting down database connection.");
+      conn.close();
+
+      sw.stop();
+      logger.info("Ingest complete. Took {} ms.", sw.getTime());
+      
+      try {
+        annotator.close();
+      } catch (Exception e) {
+        logger.error("An error occurred when closing the ConcurrentStanfordAnnotator object.", e);
       }
       
-      logger.info("Tasks submitted. Preparing SQL inserts.");
-      while(kPending != 0) {
-        logger.info("Waiting on next document in driver...");
-        // c = annotator.srv.poll(60 * 3, TimeUnit.SECONDS);
-        Future<Communication> c = annotator.srv.poll(60 * 3, TimeUnit.SECONDS);
-        Communication ac = c.get();
-        logger.debug("Retrieved communication: {}", ac.getId());
-        kPending--;
-        try (PreparedStatement ps = conn.prepareStatement("INSERT INTO documents (id, bytez) VALUES (?,?)");) {
-          ps.setString(1, ac.getId());
-          ps.setBytes(2, cs.toBytes(ac));
-          ps.executeUpdate();
-          kProcessed++;
-          
-          if (kProcessed % 100 == 0) {
-            logger.info("Converted {} documents; committing.", kProcessed);
-            conn.commit();
-          }
-        } catch (SQLException e) {
-          logger.error("Got SQL Exception.", e);
-        }
-      }
-    }
-
-    logger.info("Database transaction prepared. Committing.");
-    conn.commit();
-    logger.info("Committed successfully. Shutting down database connection.");
-    conn.close();
-
-    sw.stop();
-    logger.info("Ingest complete. Took {} ms.", sw.getTime());
-    annotator.close();
-    System.setErr(err);
+      System.setErr(err);
+    } catch (SQLException ex) {
+      logger.error("An SQLException was caught while processing the connection.", ex);
+    } catch (InterruptedException ex) {
+      logger.error("An InterruptedException was caught while processing documents.", ex);
+    } catch (ExecutionException e) {
+      logger.error("An ExecutionException was caught during task submission or queue retrieval.", e);
+    } 
   }
 
   @Override
