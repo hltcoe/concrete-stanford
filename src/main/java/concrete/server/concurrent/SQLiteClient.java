eae28849a388a62264bae7590aa7828af9de9961
max thomas <maxjthomas@gmail.com>
2014-09-12 17:03:59 -0400

    Begin to add SQLite client.

---
+++ src/main/java/concrete/server/concurrent/SQLiteClient.java
@@ -0,0 +1,25 @@
+/*
+ * 
+ */
+package concrete.server.concurrent;
+
+/**
+ * @author max
+ *
+ */
+public class SQLiteClient implements AutoCloseable {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void close() throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+}
