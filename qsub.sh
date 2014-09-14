2a985e0796f6313e230c07212cbb193dec3cc4d2
Max Thomas <max.thomas@jhuapl.edu>
2014-09-13 21:37:59 -0400

    Move to qsub.sh

---
+++ qsub.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#$ -cwd                        # run from current working directory
+#$ -j y                        # join stderr to stdout
+#$ -V                          # job has the same environment variables as the submission shell
+#$ -l h_rt=50:00:00            # runtime limit
+#$ -l mem_free=15G             # expected amount of mem
+#$ -l num_proc=2
+#$ -M maxjthomas@gmail.com
+#$ -o /home/hltcoe/mthomas/job-logs/agiga2    # log here
+#$ -m ase                       # a=aborted b=begining e=end s=suspended
+#$ -S /bin/bash
+#$ -N agiga2                    # job name
+#$ -b y                         # run command line vs batch (?)
+echo "Got argument: $1"
+java \
+    -XX:+UseSerialGC \
+    -Xmx10G \
+    -Dlog4j.configurationFile=/home/hltcoe/mthomas/git/concrete-stanford/src/main/resources/log4j2.json \
+    -cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.3.2-SNAPSHOT-jar-with-dependencies.jar \
+    concrete.server.concurrent.QSubbableStanfordConverter $1
--- runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-java \
-    -XX:+UseSerialGC \
-    -Dlog4j.configurationFile=/home/hltcoe/mthomas/git/concrete-stanford/src/main/resources/log4j2.json \
-    -cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.3.2-SNAPSHOT-jar-with-dependencies.jar \
-    concrete.server.concurrent.RedisEnabledStanfordConverter localhost 45445 $1
