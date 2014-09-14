8d4f3813ee74ad206e449b11662e4d3f9c923c66
Max Thomas <maxjthomas@gmail.com>
2014-09-13 20:30:48 -0400

    Avoid blocking DB by a single mapper.

--- src/main/java/concrete/server/concurrent/RedisEnabledStanfordConverter.java
+++ src/main/java/concrete/server/concurrent/RedisEnabledStanfordConverter.java
@@ -7,7 +7,13 @@ package concrete.server.concurrent;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
 
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
@@ -33,7 +39,7 @@ public class RedisEnabledStanfordConverter {
   public static final String FINISHED_KEY = "gigaword-finished-keys";
 
   private static final Logger logger = LoggerFactory.getLogger(RedisEnabledStanfordConverter.class);
-  
+
   /**
    * 
    */
@@ -50,13 +56,13 @@ public class RedisEnabledStanfordConverter {
 
     SystemErrDisabler sed = new SystemErrDisabler();
     sed.disable();
-    
+
     ClojureIngester ci = new ClojureIngester();
     StanfordAgigaPipe pipe = new StanfordAgigaPipe();
-    
+
+    Map<String, Communication> idToCommMap = new HashMap<>(1000);
     JedisPool jp = new JedisPool(redisHost, redisPort);
-    try (Jedis jedis = jp.getResource();
-        SQLiteClient sqlcli = new SQLiteClient(args[2])) {
+    try (Jedis jedis = jp.getResource();) {
       Optional<String> idToProcess = Optional.ofNullable(jedis.spop(REDIS_SET_KEY));
       while (idToProcess.isPresent()) {
         String idToGrab = idToProcess.get();
@@ -64,31 +70,77 @@ public class RedisEnabledStanfordConverter {
         try {
           Communication wSections = ci.proxyDocStringToProxyDoc(document).sectionedCommunication();
           Communication postStanford = pipe.process(wSections);
-          sqlcli.insert(postStanford);
-          jedis.sadd(FINISHED_KEY, idToGrab);
+          idToCommMap.put(idToGrab, postStanford);
+          // sqlcli.insert(postStanford);
+          // jedis.sadd(FINISHED_KEY, idToGrab);
         } catch (IOException | TException | ConcreteException | AnnotationException e) {
-          logger.error("Caught an exception while annotating a document.", e);
-          logger.error("Document in question: {}", idToGrab);
+          logger.warn("Caught an exception while annotating a document.", e);
+          logger.warn("Document in question: {}", idToGrab);
           // Put key in error set.
-          try (Jedis nj = jp.getResource()) {
-            nj.sadd(ERRORS_KEY, idToGrab);
+          jedis.sadd(ERRORS_KEY, idToGrab);
+        }
+
+        if (idToCommMap.size() == 100) {
+          int connAttemptsRemaining = 10;
+          while (connAttemptsRemaining > 0) {
+            connAttemptsRemaining--;
+            Set<Entry<String, Communication>> eSet = new HashSet<>();
+            try (SQLiteClient sqlcli = new SQLiteClient(args[2], 101)) {
+              Iterator<Entry<String, Communication>> iter = idToCommMap.entrySet().iterator();
+              while (iter.hasNext()) {
+                Entry<String, Communication> entry = iter.next();
+                String docId = entry.getKey();
+                Communication annotated = entry.getValue();
+                try {
+                  sqlcli.insert(annotated);
+                  jedis.sadd(FINISHED_KEY, docId);
+                  eSet.add(entry);
+                } catch (ConcreteException e) {
+                  logger.warn("Caught a ConcreteException during writing to the DB.", e);
+                  jedis.sadd(ERRORS_KEY, docId);
+                  iter.remove();
+                }
+              }
+
+            } catch (SQLException se) {
+              logger.warn("Caught SQLException; backing off and trying again.", se);
+              try {
+                Thread.sleep(2 * (2 * (10 - connAttemptsRemaining)) * 1000);
+              } catch (InterruptedException e) {
+                logger.warn("Sleep messed up (won't happen)");
+              }
+            }
+
+            // Remove committed docs, then retry if needed.
+            idToCommMap.entrySet().removeAll(eSet);
+            // Reaching this point means that the iteration has finished.
+            // Check size. If 0, break out of SQL loop.
+            if (idToCommMap.size() == 0)
+              break;
+            else
+              logger.info("Attempting to reconnect.");
           }
 
-        } catch (SQLException se) {
-          logger.error("Caught a SQLException when inserting document.", se);
-          logger.error("Document in question: {}", idToGrab);
-          try (Jedis nj = jp.getResource()) {
-            nj.sadd(ERRORS_KEY, idToGrab);
+          if (connAttemptsRemaining == 0 && idToCommMap.size() > 0) {
+            logger.error("Failed to connect to the SQL database after 10 tries. Crashing.");
+            idToCommMap.keySet().forEach(i -> jedis.sadd(ERRORS_KEY, i));
+            System.exit(1);
           }
         }
-        
+
         idToProcess = Optional.ofNullable(jedis.spop(REDIS_SET_KEY));
       }
-    } catch (SQLException e1) {
-      logger.error("Caught an exception when opening or closing the SQLiteClient.", e1);
     }
-    
+
     sed.enable();
     jp.destroy();
   }
 }
+
+// catch (SQLException se) {
+// logger.error("Caught a SQLException when inserting document.", se);
+// logger.error("Document in question: {}", idToGrab);
+// try (Jedis nj = jp.getResource()) {
+// nj.sadd(ERRORS_KEY, idToGrab);
+// }
+// }
