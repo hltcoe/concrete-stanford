6c825899f5117415bd4b68e43517ad3ec553bc1a
Max Thomas <max.thomas@jhuapl.edu>
2014-09-13 21:37:15 -0400

    Fix runner script. Still getting some weird error.

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java \
--XX:+UseSerialGC \
--Dlog4j.configurationFile=/home/hltcoe/mthomas/git/concrete-stanford/src/main/resources/log4j2.json \
--cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.3.2-SNAPSHOT-jar-with-dependencies.jar \
-concrete.server.concurrent.RedisEnabledStanfordConverter localhost 45445 /home/hltcoe/mthomas/data/giga/gigatwo.db
+    -XX:+UseSerialGC \
+    -Dlog4j.configurationFile=/home/hltcoe/mthomas/git/concrete-stanford/src/main/resources/log4j2.json \
+    -cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.3.2-SNAPSHOT-jar-with-dependencies.jar \
+    concrete.server.concurrent.RedisEnabledStanfordConverter localhost 45445 $1
