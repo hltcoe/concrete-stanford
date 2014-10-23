ee3c34e9c34a9ebbd5e790b116c4965853a18708
Max Thomas <max.thomas@jhuapl.edu>
2014-10-23 15:59:27 -0400

    Updates for latest agiga and such

--- qsub.sh
+++ qsub.sh
@@ -3,17 +3,17 @@
 #$ -j y                        # join stderr to stdout
 #$ -V                          # job has the same environment variables as the submission shell
 #$ -l h_rt=50:00:00            # runtime limit
-#$ -l mem_free=15G             # expected amount of mem
-#$ -l num_proc=2
+#$ -l mem_free=8G             # expected amount of mem
+#$ -l num_proc=1
 #$ -M max.thomas@jhuapl.edu
 #$ -o /home/hltcoe/mthomas/job-logs/agiga2-rc5    # log here
 #$ -m ase                       # a=aborted b=begining e=end s=suspended
 #$ -S /bin/bash
-#$ -N agiga2                    # job name
+#$ -N agiga2-with-conc-agiga-fixes                    # job name
 #$ -b y                         # run command line vs batch (?)
 java \
     -XX:+UseSerialGC \
-    -Xmx10G \
+    -Xmx7G \
     -Dlog4j.configurationFile=/home/hltcoe/mthomas/git/concrete-stanford/src/main/resources/log4j2.json \
-    -cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.7.0-jar-with-dependencies.jar \
+    -cp /home/hltcoe/mthomas/git/concrete-stanford/target/concrete-stanford-3.7.1-SNAPSHOT-jar-with-dependencies.jar \
     concrete.server.concurrent.PostgresEnabledQSubbableStanfordConverter
