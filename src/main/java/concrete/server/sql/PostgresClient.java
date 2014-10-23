3760196e0dd9a9b0e2367aabaf232ce90c3842a0
Max Thomas <max.thomas@jhuapl.edu>
2014-10-23 13:14:38 -0400

    Change cursor size.

--- src/main/java/concrete/server/sql/PostgresClient.java
+++ src/main/java/concrete/server/sql/PostgresClient.java
@@ -35,62 +35,62 @@ import edu.jhu.hlt.gigaword.ClojureIngester;
 public class PostgresClient implements AutoCloseable {
 
   private static final Logger logger = LoggerFactory.getLogger(PostgresClient.class);
-  
+
   public static final String DOCUMENTS_TABLE = "documents_raw";
   public static final String ANNOTATED_TABLE = "annotated";
-  
+
   private final String randQuery = "SELECT raw FROM " + DOCUMENTS_TABLE + " WHERE id NOT IN"
        + " (SELECT documents_id FROM " + ANNOTATED_TABLE + " ) AND random() < 0.01 LIMIT 100";
-  
+
   private final String host;
   private final String dbName;
   private final String userName;
   private final byte[] pass;
-  
+
   private final CommunicationSerialization cs = new CommunicationSerialization();
   private final Connection conn;
-  
+
   private final PreparedStatement insertPS;
   private final PreparedStatement isAnnotatedPS;
   private final PreparedStatement getDocumentPS;
   private final PreparedStatement nextCommPS;
-  
+
   private final ClojureIngester ci = new ClojureIngester();
-  
+
   /**
-   * @throws SQLException 
-   * 
+   * @throws SQLException
+   *
    */
   public PostgresClient(String host, String dbName, String userName, byte[] pass) throws SQLException {
     IFn req = Clojure.var("clojure.core", "require");
     // req.invoke(Clojure.read("gigaword-ingester.giga"));
     req.invoke(Clojure.read("gigaword-ingester.giga"));
-    
+
     this.host = host;
     this.dbName = dbName;
     this.userName = userName;
     this.pass = pass;
-    
+
     this.conn = this.getConnector();
     this.insertPS = this.conn.prepareStatement("INSERT INTO " + ANNOTATED_TABLE + " (documents_id, bytez) VALUES (?,?)");
     this.isAnnotatedPS = this.conn.prepareStatement("SELECT documents_id FROM annotated WHERE documents_id = ?");
     this.getDocumentPS = this.conn.prepareStatement("SELECT raw FROM documents_raw WHERE id = ?");
     this.nextCommPS = this.conn.prepareStatement(randQuery);
   }
-  
+
   public boolean isDocumentAnnotated(String id) throws SQLException {
     this.isAnnotatedPS.setString(1, id);
     try (ResultSet rs = this.isAnnotatedPS.executeQuery();) {
       return rs.next();
     }
   }
-  
+
   public boolean availableUnannotatedCommunications() throws SQLException {
     try (ResultSet rs = this.nextCommPS.executeQuery()) {
       return rs.next();
     }
   }
-  
+
   public ProxyCommunication getUnannotatedCommunication() throws SQLException {
     try (ResultSet rs = this.nextCommPS.executeQuery()) {
       if (rs.next()) {
@@ -101,7 +101,7 @@ public class PostgresClient implements AutoCloseable {
       }
     }
   }
-  
+
   public ProxyCommunication getDocument(String id) throws SQLException {
     this.getDocumentPS.setString(1, id);
     try (ResultSet rs = this.getDocumentPS.executeQuery()) {
@@ -118,10 +118,10 @@ public class PostgresClient implements AutoCloseable {
     Properties props = new Properties();
     props.setProperty("user", this.userName);
     props.setProperty("password", new String(pass));
-    
+
     return DriverManager.getConnection("jdbc:postgresql://" + this.host + "/" + this.dbName, props);
   }
-  
+
   public void insertCommunication(Communication c) throws SQLException {
     String docId = c.getId();
     try {
@@ -129,7 +129,7 @@ public class PostgresClient implements AutoCloseable {
         logger.info("Beat to the punch: {} already annotated.", docId);
         return;
       }
-      
+
       this.insertPS.setString(1, docId);
       this.insertPS.setBytes(2, this.cs.toBytes(c));
       this.insertPS.executeUpdate();
@@ -146,7 +146,7 @@ public class PostgresClient implements AutoCloseable {
 
   public Set<String> getIngestedDocIds() throws SQLException {
     Set<String> idSet = new HashSet<String>(12000000);
-    
+
     try (Connection conn = this.getConnector();
         PreparedStatement ps = conn.prepareStatement("SELECT id FROM documents_raw");) {
       ResultSet rs = ps.executeQuery();
@@ -155,10 +155,10 @@ public class PostgresClient implements AutoCloseable {
         idSet.add(id);
       }
     }
-    
+
     return idSet;
   }
-  
+
   public Communication getSingleDocument() throws SQLException, ConcreteException {
     try (Connection conn = this.getConnector();
         PreparedStatement ps = conn.prepareStatement("SELECT bytez FROM annotated LIMIT 1");) {
@@ -171,13 +171,13 @@ public class PostgresClient implements AutoCloseable {
       }
     }
   }
-  
+
   public int countNumberAnnotatedSentences() throws Exception {
     try (Connection conn = this.getConnector();
         PreparedStatement ps = conn.prepareStatement("SELECT * FROM annotated");
         ConcurrentCommunicationLoader loader = new ConcurrentCommunicationLoader()) {
       conn.setAutoCommit(false);
-      ps.setFetchSize(1000);
+      ps.setFetchSize(100);
       int nSentences = 0;
       int docCounter = 0;
       try (ResultSet rs = ps.executeQuery()) {
@@ -186,12 +186,12 @@ public class PostgresClient implements AutoCloseable {
           Communication c = this.cs.fromBytes(bytes);
           nSentences += new SuperCommunication(c).generateSentenceIdToSectionMap().values().size();
           docCounter++;
-          
+
           if (docCounter % 10000 == 0)
             logger.info("Finished {} docs.", docCounter);
         }
       }
-      
+
       return nSentences;
     }
   }
@@ -204,13 +204,13 @@ public class PostgresClient implements AutoCloseable {
     logger.debug("Closing.");
     this.conn.close();
   }
-  
+
   public static void main (String... args) {
     if (args.length != 1) {
       logger.info("This program takes 1 argument: a path on disk to serialize a Communication object.");
       System.exit(1);
     }
-    
+
     Optional<String> psqlHost = Optional.ofNullable(System.getenv("HURRICANE_HOST"));
     Optional<String> psqlDBName = Optional.ofNullable(System.getenv("HURRICANE_DB"));
     Optional<String> psqlUser = Optional.ofNullable(System.getenv("HURRICANE_USER"));
@@ -224,7 +224,7 @@ public class PostgresClient implements AutoCloseable {
       logger.info("HURRICANE_PASS : password for user");
       System.exit(1);
     }
-    
+
     try (PostgresClient cli = new PostgresClient(psqlHost.get(), psqlDBName.get(), psqlUser.get(), psqlPass.get().getBytes())) {
       new SuperCommunication(cli.getSingleDocument()).writeToFile(args[0], true);
     } catch (SQLException | ConcreteException e) {
