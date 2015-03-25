d7642de0bf59a849415bccd060a0278aa31e8fa9
max thomas <max.thomas@jhu.edu>
2015-03-25 02:34:52 -0400

    Tests kinda pass

--- src/test/java/edu/jhu/hlt/concrete/stanford/NonPTBTextTest.java
+++ src/test/java/edu/jhu/hlt/concrete/stanford/NonPTBTextTest.java
@@ -1,55 +1,26 @@
 package edu.jhu.hlt.concrete.stanford;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.apache.thrift.TException;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import concrete.agiga.util.ConcreteAgigaProperties;
-import concrete.tools.AnnotationException;
 import concrete.validation.CommunicationValidator;
 import edu.jhu.hlt.concrete.AnnotationMetadata;
 import edu.jhu.hlt.concrete.Communication;
-import edu.jhu.hlt.concrete.DependencyParse;
-import edu.jhu.hlt.concrete.Entity;
-import edu.jhu.hlt.concrete.EntityMention;
-import edu.jhu.hlt.concrete.EntityMentionSet;
-import edu.jhu.hlt.concrete.EntitySet;
-import edu.jhu.hlt.concrete.Parse;
 import edu.jhu.hlt.concrete.Section;
 import edu.jhu.hlt.concrete.Sentence;
 import edu.jhu.hlt.concrete.TextSpan;
 import edu.jhu.hlt.concrete.Token;
 import edu.jhu.hlt.concrete.TokenList;
-import edu.jhu.hlt.concrete.TokenTagging;
 import edu.jhu.hlt.concrete.Tokenization;
 import edu.jhu.hlt.concrete.TokenizationKind;
-import edu.jhu.hlt.concrete.communications.SuperCommunication;
-import edu.jhu.hlt.concrete.gigaword.ConcreteGigawordDocumentFactory;
-import edu.jhu.hlt.concrete.serialization.CommunicationSerializer;
-import edu.jhu.hlt.concrete.serialization.CompactCommunicationSerializer;
-import edu.jhu.hlt.concrete.stanford.AnnotateTokenizedConcrete;
-import edu.jhu.hlt.concrete.util.ConcreteException;
-import edu.jhu.hlt.concrete.util.ConcreteFactory;
-import edu.jhu.hlt.concrete.util.ConcreteUUIDFactory;
-import edu.jhu.hlt.concrete.util.SuperTextSpan;
-import gigaword.api.GigawordDocumentConverter;
-import gigaword.interfaces.GigawordDocument;
+import edu.jhu.hlt.concrete.random.RandomConcreteFactory;
+import edu.jhu.hlt.concrete.uuid.UUIDFactory;
 
 public class NonPTBTextTest {
-  private static final Logger logger = LoggerFactory.getLogger(StanfordAgigaPipeTest.class);
-  ConcreteFactory cf = new ConcreteFactory();
-  ConcreteUUIDFactory cuf = new ConcreteUUIDFactory();
+  RandomConcreteFactory cf = new RandomConcreteFactory();
 
   //public static final String chineseText1 = "德国 工程 集团 西门子 和 瑞典 能源 公司 Vattenfall 已 将 邯峰 Hanfeng 火力 发电厂 40%  的 股份 转让 给 中国 华 能 集团 ChinaHuanengGroup 和 中信 CITIC .";
   //public static final String chineseText1 = "德国 工程 集团 西门子 和 瑞典 能源 公司 Vattenfall 已 将 邯峰 ( Hanfeng ) 火力 发电厂 40%  的 股份 转让 给 中国 华 能 集团 ( ChinaHuanengGroup ) 和 中信 ( CITIC ) .";
@@ -59,17 +30,17 @@ public class NonPTBTextTest {
 
   @Test
   public void testChinese1() throws Exception {
-    Communication chineseComm = this.cf.randomCommunication().setText(chineseText1); 
+    Communication chineseComm = this.cf.communication().setText(chineseText1); 
     AnnotationMetadata md = new AnnotationMetadata().
       setTool("concrete-stanford:test").setTimestamp(System.currentTimeMillis() / 1000);
     chineseComm.setMetadata(md);
-    Section section = new Section().setUuid(cuf.getConcreteUUID())
+    Section section = new Section().setUuid(UUIDFactory.newUUID())
       .setTextSpan(new TextSpan().setStart(0).setEnding(chineseText1.length())).setKind("</TEXT>");
     chineseComm.addToSectionList(section);
-    Sentence sentence = new Sentence().setUuid(cuf.getConcreteUUID())
+    Sentence sentence = new Sentence().setUuid(UUIDFactory.newUUID())
       .setTextSpan(new TextSpan().setStart(0).setEnding(chineseText1.length()));
     section.addToSentenceList(sentence);
-    Tokenization tokenization = new Tokenization().setUuid(cuf.getConcreteUUID())
+    Tokenization tokenization = new Tokenization().setUuid(UUIDFactory.newUUID())
       .setMetadata(md).setKind(TokenizationKind.TOKEN_LIST);
     TokenList tokenList = new TokenList();
     int tokId = 0;
@@ -92,17 +63,17 @@ public class NonPTBTextTest {
 
   @Test
   public void testEnglish1() throws Exception {
-    Communication englishComm = this.cf.randomCommunication().setText(englishText1); 
+    Communication englishComm = this.cf.communication().setText(englishText1); 
     AnnotationMetadata md = new AnnotationMetadata().
       setTool("concrete-stanford:test").setTimestamp(System.currentTimeMillis() / 1000);
     englishComm.setMetadata(md);
-    Section section = new Section().setUuid(cuf.getConcreteUUID())
+    Section section = new Section().setUuid(UUIDFactory.newUUID())
       .setTextSpan(new TextSpan().setStart(0).setEnding(englishText1.length())).setKind("Passage");
     englishComm.addToSectionList(section);
-    Sentence sentence = new Sentence().setUuid(cuf.getConcreteUUID())
+    Sentence sentence = new Sentence().setUuid(UUIDFactory.newUUID())
       .setTextSpan(new TextSpan().setStart(0).setEnding(englishText1.length()));
     section.addToSentenceList(sentence);
-    Tokenization tokenization = new Tokenization().setUuid(cuf.getConcreteUUID())
+    Tokenization tokenization = new Tokenization().setUuid(UUIDFactory.newUUID())
       .setMetadata(md).setKind(TokenizationKind.TOKEN_LIST);
     TokenList tokenList = new TokenList();
     int tokId = 0;
