131952a51efe2e73ddc40f2cad4722b17634aa6e
Matt Gormley <mrg@cs.jhu.edu>
2015-05-28 14:49:30 -0400

    Exposing bug in ConcreteAnnotator (via AnnotateTokenizedConcrete) by changing the english input text and tokenizing carefully.

--- src/test/java/edu/jhu/hlt/concrete/stanford/NonPTBTextTest.java
+++ src/test/java/edu/jhu/hlt/concrete/stanford/NonPTBTextTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -31,7 +33,8 @@ public class NonPTBTextTest {
   RandomConcreteFactory cf = new RandomConcreteFactory();
 
   public static final String chineseText1 = "德国 工程 集团 西门子 和 瑞典 能源 公司 Vattenfall 已 将 邯峰 ( Hanfeng ) 火力 发电厂 40%  的 股份 转让 给 中国 华 能 集团 ( ChinaHuanengGroup ) 和 中信 ( CITIC ) .";
-  public static final String englishText1 = "John ( a boy ) ran fast .";
+  // Note in the English sentence below, we have carefully included extra whitespace at the start, middle, and end of the sentence.
+  public static final String englishText1 = "   John ( a boy )    ran fast .  ";
 
   @Test
   public void testChinese1() throws Exception {
@@ -83,29 +86,35 @@ public class NonPTBTextTest {
         "concrete-stanford:test").setTimestamp(
         System.currentTimeMillis() / 1000);
     englishComm.setMetadata(md);
+    // Note: the section excludes the first whitespace.
+    assertEquals(" ", englishText1.substring(0, 1)); // This is an expectation, not a test.
     Section section = new Section()
         .setUuid(UUIDFactory.newUUID())
         .setTextSpan(
-            new TextSpan().setStart(0).setEnding(englishText1.length()))
+            new TextSpan().setStart(1).setEnding(englishText1.length()))
         .setKind("Passage");
     englishComm.addToSectionList(section);
+    // Note: the sentence excludes the first two whitespaces.
+    assertEquals("  ", englishText1.substring(0, 2)); // This is an expectation, not a test.
     Sentence sentence = new Sentence().setUuid(UUIDFactory.newUUID())
         .setTextSpan(
-            new TextSpan().setStart(0).setEnding(englishText1.length()));
+            new TextSpan().setStart(2).setEnding(englishText1.length()));
     section.addToSentenceList(sentence);
     Tokenization tokenization = new Tokenization()
         .setUuid(UUIDFactory.newUUID()).setMetadata(md)
         .setKind(TokenizationKind.TOKEN_LIST);
     TokenList tokenList = new TokenList();
     int tokId = 0;
-    int tokenStart = 0, tokenEnd = 0;
-    for (String tokenStr : englishText1.split(" +")) {
-      tokenEnd += tokenStr.length();
-      Token token = new Token().setTokenIndex(tokId++).setText(tokenStr)
-          .setTextSpan(new TextSpan().setStart(tokenStart).setEnding(tokenEnd));
-      tokenStart = tokenEnd + 1;
-      tokenEnd = tokenStart;
-      tokenList.addToTokenList(token);
+    Pattern whitespace = Pattern.compile("[^ ]+");
+    Matcher tokMatcher = whitespace.matcher(englishText1);
+    while (tokMatcher.find()) {
+        String tokenStr = tokMatcher.group();
+        int tokenStart = tokMatcher.start();
+        int tokenEnd = tokMatcher.end();
+        System.out.printf("token: id=%d start=%d end=%d str=%s\n", tokId, tokenStart, tokenEnd, tokenStr);
+        Token token = new Token().setTokenIndex(tokId++).setText(tokenStr)
+                .setTextSpan(new TextSpan().setStart(tokenStart).setEnding(tokenEnd));
+        tokenList.addToTokenList(token);
     }
     tokenization.setTokenList(tokenList);
     tokenization.setTokenTaggingList(new ArrayList<>());
@@ -113,8 +122,7 @@ public class NonPTBTextTest {
     assertTrue(new CommunicationValidator(englishComm).validate());
     AnnotateTokenizedConcrete atc = new AnnotateTokenizedConcrete(PipelineLanguage.ENGLISH);
     atc.annotateWithStanfordNlp(englishComm);
-    String[] expectedTokens = englishText1.split(" +");
-    assertEquals(expectedTokens.length, tokenList.getTokenListSize());
+    assertEquals(8, tokenList.getTokenListSize());
     assertTrue(tokenization.isSetParseList());
     assertEquals(1, tokenization.getParseListSize());
     int maxCParseSpan = 0;
@@ -124,17 +132,20 @@ public class NonPTBTextTest {
           : maxCParseSpan;
     }
     assertEquals(8, maxCParseSpan);
+    
+    assertEquals("John", tokenList.getTokenList().get(0).getText());
     assertEquals("(", tokenList.getTokenList().get(1).getText());
-    int tokIdx = 0;
-    tokenStart = 0;
-    tokenEnd = 0;
-    for (String tokenStr : expectedTokens) {
-      assertEquals(tokenStr, tokenList.getTokenList().get(tokIdx).getText());
-      tokenEnd += tokenStr.length();
-      assertEquals(tokenStr, englishText1.substring(tokenStart, tokenEnd));
-      tokenStart = tokenEnd + 1;
-      tokenEnd = tokenStart;
-      ++tokIdx;
-    }
+    assertEquals("run", tokenList.getTokenList().get(5).getText());
+    assertEquals("fast", tokenList.getTokenList().get(6).getText());
+    
+    TextSpan ts;  
+    ts = tokenList.getTokenList().get(0).getTextSpan();
+    assertEquals("John", englishText1.substring(ts.getStart(), ts.getEnding()));
+    ts = tokenList.getTokenList().get(1).getTextSpan();
+    assertEquals("(", englishText1.substring(ts.getStart(), ts.getEnding()));
+    ts = tokenList.getTokenList().get(5).getTextSpan();
+    assertEquals("run", englishText1.substring(ts.getStart(), ts.getEnding()));
+    ts = tokenList.getTokenList().get(6).getTextSpan();
+    assertEquals("fast", englishText1.substring(ts.getStart(), ts.getEnding()));
   }
 }
